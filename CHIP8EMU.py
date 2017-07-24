495aca15d4ade5e07937aa3b22b0830801edc3ae
Pedrocasf <pedrostarling2000@gmail.com>
2017-07-24 00:02:10 -0300

    23/07/2017 23:59 and 24/07/2017 00:00 MAZE defeated Signed-off-by: Pedrocasf <pedrostarling2000@gmail.com>

--- CHIP8EMU.py
+++ CHIP8EMU.py
@@ -4,16 +4,15 @@ import random
 import numpy as np
 pygame.init()
 class cpu():
-	key_inputs = [0]*16
 	screen = pygame.display.set_mode((640,320)) 
 	memory = [0]*4096
 	i = 0
 	V =[0]*16
-	sound_timer = 0
-	delay_timer = 0
+	st = 0
+	dt = 0
 	VI = 0
-	pc = 0x200 
-	stack = [0] 
+	pc = 0x200
+	stack = [0] *16
 	sp = 0
 	x = 0
 	y = 0
@@ -39,7 +38,7 @@ class cpu():
 		memory[i] = fontset[i]
 		i += 1
 	
-	with open('games/'+"PONG","rb") as f:
+	with open('games/'+"MAZE","rb") as f:
 		i = 0
 		rom = f.read()
 		
@@ -58,54 +57,53 @@ class cpu():
 		opcode = str(opcode)
 		print (V)
 		print(opcode)
+		pc +=2
 		if opcode [:3] == "0x0":
-			if opcode == "e00":
+			if opcode == "0x00e0":
 				screen.fill((0,0,0))
-				pc += 2
-			elif opcode == "0ee":
+				
+			elif opcode == "0x00ee":
 				sp -= 1 
 				pc = stack[0]
-				pc += 2
+				
 			
 		elif opcode[:3] == "0x1":
 			pc = opcode [3:] 
 			pc = int(pc,16)
-			pc += 2
+			
 			
 		elif opcode [:3] == "0x2":
-			stack[sp] = pc
-			sp += 1
+			sp +=1
+			stack [0] = pc
 			pc = int(opcode[3:],16)
 			
 		elif opcode[:3] == "0x3":
 			x = int(opcode[3],16)
 			if int(hex(V[x])[2:],16) == int(str(opcode [4:]),16):
-				pc += 4
-			else :
-				pc += 2
-			
+				pc +=  2
+				
 		elif opcode[:3] == "0x4":
 			x = int(opcode[3],16)
 			if int(hex(V[x])[2:],16) != int(str(opcode [4:]),16):
 				pc += 2
-			pc += 2
+			
 			
 		elif opcode[:3] == "0x5":
 			x = int(opcode[3],16)
 			y = int(opcode[4],16)
 			if V[x] == V[y]:
 				pc += 2
-			pc +=2
+			
 			
 		elif opcode [:3] == "0x6":
 			x = int(opcode[3],16)
 			V[x] = int(opcode[4:],16)
-			pc += 2
+			
 			
 		elif opcode[:3] == "0x7":
 			x = int(opcode [3],16)
 			V[x] = V[x] + int(opcode [4:],16)
-			pc += 2
+			
 			
 		elif opcode[:3] == "0x8":
 			
@@ -113,59 +111,66 @@ class cpu():
 				x = int(opcode[3],16)
 				y = int(opcode [4],16)
 				V[x] = V[y]
-				pc += 2
+				
 			
 			elif opcode [5] == "1":
 				x = int(opcode[3],16)
 				y = int (opcode[4],16)
 				V[x] = V[x] | V[y]
-				pc +=2
+				
 				
 			elif opcode [5] == "2":
-				pc +=2
+				x = int(opcode[3],16)
+				y = int(opcode[4],16)
+				V[x] = V[x] and V[y]
 				
 			elif opcode [5] == "3":
-				pc +=2
+				x = int(opcode[3],16)
+				y = int(opcode[4],16)
+				V[x] = V[x] ^ V[y]
 				
 			elif opcode [5] == "4":
-				pc +=2
+				x = int(opcode[3],16)
+				y = int(opcode[4],16)
 				
 			elif opcode [5] == "5":
-				pc +=2
+				x = int(opcode[3],16)
+				y = int(opcode[4],16)
 				
 			elif opcode [5] == "6":
-				pc +=2
+				x = int(opcode[3],16)
+				y = int(opcode[4],16)
 				
 			elif opcode [5] == "7":
-				pc +=2
+				x = int(opcode[3],16)
+				y = int(opcode[4],16)
 				
 			elif opcode [5] == "e":
-				pc +=2
+				x = int(opcode[3],16)
+				y = int(opcode[4],16)
 				
 		elif opcode[:3] == "0x9":
-			pc +=2
-			
+			x = int(opcode[3],16)
+			y = int(opcode[4],16)
+			if V[x] != V[y]:
+				pc += 2
 		elif opcode[:3] == "0xa":
 			VI = int(opcode[3:],16)
-			print(VI)
-			pc += 2
+			
 		
 		elif opcode[:3] == "0xb":
-			pc += 2
+			pc = int(opcode[3:],16)+V[0]
 			
 		elif opcode[:3] == "0xc":
-			
 			x = int(opcode[3],16)
-			V[x] =  random.randint(0,int(opcode[4:]))
-			pc += 2
+			V[x] =  random.randint(0,255) & int(opcode[4:])
+			
 		
 		elif opcode[:3] == "0xd":
-			
 			n = int(opcode[5],16)
 			x = int (opcode[3],16)
 			y = int(opcode[4],16)
 			V[0xf] = 0
-			WHITE=(255,255,255)
 			saved_pc = pc
 			pc = VI
 			saved_VI = VI
@@ -182,38 +187,41 @@ class cpu():
 				sprite_buffer.extend(es)
 			sprite_buffer = np.asarray(sprite_buffer,dtype=int).reshape(n,8)
 			sprite_buffer = sprite_buffer * 255
+			print (sprite_buffer)
 			surface = pygame.pixelcopy.make_surface(sprite_buffer)
 			surface = pygame.transform.scale(surface,(n*10,80))
 			screen.blit(surface,(V[x]*10,V[y]*10))
 			pygame.display.update()
 			VI = saved_VI 	
-			pc = saved_pc + 2
+			pc = saved_pc
 
 		elif opcode[:3] == "0xe":
 			
 			if opcode [4:] == "9e":
-				pc += 2
+				if key[V[x]] == True:
+					pc += 2
 			elif opcode [4:] == "a1":
-				pc += 2
-			elif opcode [2:] == "e0":
-				pc += 2
+				if key[V[x]] == False:
+					pc += 2
+				
 		elif opcode[:3] == "0xf":
 			
 			if opcode[4:] == "07":
-				pc += 2
+				x = int(opcode[3],16)
+				V[x] = dt
 			elif opcode[4:] == "0a":
-				pc += 2
+				 x = int(opcode[3],16)
 			elif opcode[4:] == "15":
-				pc += 2	
+				x = int(opcode[3],16)
 			elif opcode[4:] == "18":
-				pc += 2	
+				x = int(opcode[3],16)
 			elif opcode[4:] == "1e":
-				pc += 2	
+				x = int(opcode[3],16)
 			elif opcode[4:] == "29":
-				pc += 2	
+				x = int(opcode[3],16)
 			elif opcode[4:] == "33":
-				pc += 2	
+				x = int(opcode[3],16)
 			elif opcode[4:] == "55":
-				pc += 2	
+				x = int(opcode[3],16)
 			elif opcode[4:] == "65":
-				pc += 2	
+				x = int(opcode[3],16)
