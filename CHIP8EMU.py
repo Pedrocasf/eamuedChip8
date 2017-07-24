970a2417a301700fe545d3c0a77ae3b95d8b7244
Pedrocasf <pedrostarling2000@gmail.com>
2017-07-24 01:45:13 -0300

    Most functions implemented
    Most functions implemented
    pong now shows correctly

--- CHIP8EMU.py
+++ CHIP8EMU.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 pygame.init()
 class cpu():
+	key = [0]*17
 	screen = pygame.display.set_mode((640,320)) 
 	memory = [0]*4096
 	i = 0
@@ -38,11 +39,11 @@ class cpu():
 		memory[i] = fontset[i]
 		i += 1
 	
-	with open('games/'+"MAZE2","rb") as f:
+	with open('games/'+"MAZE","rb") as f:
 		i = 0
 		rom = f.read()
 		
-		while i <= len (rom) -1: 
+		while i < len (rom) : 
 			memory[i+pc] = rom[i]
 			i += 1
 		
@@ -51,13 +52,10 @@ class cpu():
 			memory[i] = hex(memory[i])
 			memory[i] = int(memory[i], 16) 
 			i += 1
-	print (memory)
 	while pc < len(rom)+ 0x200:
 		opcode = int(hex(memory[pc]<<8),16)| int(hex(memory[pc + 1]),16)
 		opcode = hex(opcode)
 		opcode = str(opcode)
-		print (V)
-		print(opcode)
 		pc +=2
 		if opcode [:3] == "0x0":
 			if opcode == "0x00e0":
@@ -67,11 +65,9 @@ class cpu():
 				sp -= 1 
 				pc = stack[0]
 				
-			
 		elif opcode[:3] == "0x1":
 			pc = int(opcode [3:],16) 
-			
-			
+				
 		elif opcode [:3] == "0x2":
 			sp +=1
 			stack [0] = pc
@@ -122,7 +118,7 @@ class cpu():
 			elif opcode [5] == "2":
 				x = int(opcode[3],16)
 				y = int(opcode[4],16)
-				V[x] = V[x] and V[y]
+				V[x] = V[x] & V[y]
 				
 			elif opcode [5] == "3":
 				x = int(opcode[3],16)
@@ -132,23 +128,45 @@ class cpu():
 			elif opcode [5] == "4":
 				x = int(opcode[3],16)
 				y = int(opcode[4],16)
+				V[x] = V[x] + V[y]
+				if V[x] > 255:
+					V[0xf] = 1
+					V[x] = int(bin(V[x])[-4:])
 				
 			elif opcode [5] == "5":
 				x = int(opcode[3],16)
 				y = int(opcode[4],16)
+				if V[x] > V[y] :
+					V[0xf] = 1
+				else:
+					V[0xf] = 0
+				V[x] = V[x] - V[y]
 				
 			elif opcode [5] == "6":
 				x = int(opcode[3],16)
 				y = int(opcode[4],16)
-				
+				if bin(V[x])[-0:] == 1:
+					V[0xf] = 1
+				else :
+					V[0xf] = 0
+				V[x] = V[x]//2
 			elif opcode [5] == "7":
 				x = int(opcode[3],16)
 				y = int(opcode[4],16)
+				if V[y] > V[x] :
+					V[0xf] = 1
+				else:
+					V[0xf] = 0
+				V[x] = V[y] - V[x]
 				
 			elif opcode [5] == "e":
 				x = int(opcode[3],16)
 				y = int(opcode[4],16)
-				
+				if bin(V[x])[0:] == 1:
+					V[0xf] = 1
+				else :
+					V[0xf] = 0
+				V[x] = V[x]*2
 		elif opcode[:3] == "0x9":
 			x = int(opcode[3],16)
 			y = int(opcode[4],16)
@@ -157,7 +175,6 @@ class cpu():
 		elif opcode[:3] == "0xa":
 			VI = int(opcode[3:],16)
 			
-		
 		elif opcode[:3] == "0xb":
 			pc = int(opcode[3:],16)+V[0]
 			
@@ -182,15 +199,14 @@ class cpu():
 				V[y] = V[y] % 31
 			while pc < saved_VI + n :
 				VI = memory[pc]
-				print(VI)
 				es = list(bin(VI)[2:].zfill(8))	
 				pc +=1
 				sprite_buffer.extend(es)
 			sprite_buffer = np.asarray(sprite_buffer,dtype=int).reshape(n,8)
 			sprite_buffer = sprite_buffer * 255
-			
 			surface = pygame.pixelcopy.make_surface(sprite_buffer)
 			surface = pygame.transform.scale(surface,(n*10,80))
+			surface = pygame.transform.rotate(surface,90)
 			screen.blit(surface,(V[x]*10,V[y]*10))
 			pygame.display.update()
 			VI = saved_VI 	
@@ -199,30 +215,60 @@ class cpu():
 		elif opcode[:3] == "0xe":
 			
 			if opcode [4:] == "9e":
+				x = int(opcode[3],16) 
 				if key[V[x]] == True:
 					pc += 2
+					
 			elif opcode [4:] == "a1":
+				x = int(opcode[3],16)
 				if key[V[x]] == False:
 					pc += 2
+					
 				
 		elif opcode[:3] == "0xf":
 			
 			if opcode[4:] == "07":
 				x = int(opcode[3],16)
 				V[x] = dt
+				
 			elif opcode[4:] == "0a":
-				 x = int(opcode[3],16)
+				x = int(opcode[3],16)
+				while pygame.key.get_pressed() == False:
+					pass
+					
 			elif opcode[4:] == "15":
 				x = int(opcode[3],16)
+				dt = V[x]
+				
 			elif opcode[4:] == "18":
 				x = int(opcode[3],16)
+				st = V[x]
+				
 			elif opcode[4:] == "1e":
 				x = int(opcode[3],16)
+				VI = VI + V[x]
+				
 			elif opcode[4:] == "29":
 				x = int(opcode[3],16)
+				VI = V[x] *5
+				
 			elif opcode[4:] == "33":
 				x = int(opcode[3],16)
+				V[x] = str(V[x])
+				memory[VI] = int(V[x][0])
+				if len (V[x]) >= 2: 
+					memory[VI+1] = int(V[x][1])
+				if len (V[x]) >= 3:
+					memory[VI+2] = int(V[x][2])
 			elif opcode[4:] == "55":
 				x = int(opcode[3],16)
+				i = 0
+				while i < x:
+					memory[VI+i] = V[i]
+					i += 1
 			elif opcode[4:] == "65":
 				x = int(opcode[3],16)
+				i = 0
+				while i < x:
+					V[i] = memory[VI+i]
+					i += 1
